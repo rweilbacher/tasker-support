9cf63cddd9ce53f31e6f790c05ca549fafc1f470
rweilbacher <roland.weilbacher@gmai.com>
2022-03-17 14:44:31 +0100

    Initial version

---
+++ obsidian-format-helper.py
@@ -0,0 +1,45 @@
+#!/data/data/com.termux/files/usr/bin/python
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='Input text', required=True)
+parser.add_argument('-n', '--newline', help='Append new line', action="store_true")
+args = parser.parse_args()
+
+# test = "text #link *italics **bold text #eof-before-space"
+# text = test
+text = args.input
+
+new_text = ""
+i = 0
+while i < len(text):
+    # Turn tags into links
+    if text[i] == '#':
+        new_text += "[["
+        i += 1
+        while i < len(text) and (text[i] != " " or text[i] == "\n"):
+            new_text += text[i]
+            i += 1
+        new_text += "]]"
+    # Automatically close bold and italics
+    elif text[i] == '*':
+        new_text += '*'
+        suffix = '*'
+        i += 1
+        if text[i] == '*':
+            new_text += '*'
+            suffix += '*'
+            i += 1
+        while i < len(text) and text[i] != " " or text[i] == "\n":
+            new_text += text[i]
+            i += 1
+        new_text += suffix
+    else:
+        new_text += text[i]
+        i += 1
+if args.newline is True:
+    new_text += "\n"
+
+sys.stdout.write(new_text)
+sys.exit(0)
